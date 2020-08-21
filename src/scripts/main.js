1836c1828fe859a1e8774d68d0d13a47222833ae
Parker Kelley <Pklly92@gmail.com>
2020-08-21 14:46:56 -0500

    implemented the createCorn, and other create methods

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,19 @@
 console.log('Welcome to the main module');
 
 import { createPlan } from './plan.js';
+import { createCorn } from './seeds/corn.js';
+import { createSunflower } from './seeds/sunflower.js';
+import { createSoybean } from './seeds/soybean.js';
 
 const yearlyPlan = createPlan();
 
 console.log(yearlyPlan);
+
+const cornSeed = createCorn();
+console.log(cornSeed);
+
+const sunflowerSeed = createSunflower();
+console.log(sunflowerSeed);
+
+const soybeanSeed = createSoybean();
+console.log(soybeanSeed);
